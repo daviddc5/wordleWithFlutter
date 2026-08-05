37e439dc5d2b7963deeb039577976460fe8fe767
David Diaz Clifton <daviddiazclifton@gmail.com>
2026-08-05 17:28:51 +0200

    Refactor GamePage to be a StatefulWidget and update guess handling logic

--- birdle/lib/main.dart
+++ birdle/lib/main.dart
@@ -29,9 +29,14 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class GamePage extends StatelessWidget {
-  GamePage({super.key});
+class GamePage extends StatefulWidget {
+  const GamePage({super.key});
 
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
   final Game _game = Game();
 
   @override
@@ -39,19 +44,23 @@ class GamePage extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
-        spacing: 5.0,
         children: [
           for (var guess in _game.guesses)
             Row(
-              spacing: 5.0,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                for (var letter in guess) Tile(letter.char, letter.type),
+                for (var letter in guess)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 2.5, vertical: 2.5),
+                    child: Tile(letter.char, letter.type),
+                  ),
               ],
             ),
           GuessInput(
-            onSubmitGuess: (guess) {
-              // TODO, handle guess
-              print(guess);
+            onSubmitGuess: (String guess) {
+              setState(() {
+                _game.guess(guess);
+              });
             },
           ),
         ],
